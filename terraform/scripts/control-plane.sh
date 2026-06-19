a50250d46e6c08e7dba0aed59ed20f3d80b23853
olorunfemi007 <olorunfemikawonise@rocketmail.com>
2026-06-19 12:02:02 -0400

    auto join worker node and terraform destroy action

--- terraform/scripts/control-plane.sh
+++ terraform/scripts/control-plane.sh
@@ -10,11 +10,10 @@ mkdir -p /root/.kube
 cp /etc/kubernetes/admin.conf /root/.kube/config
 chown root:root /root/.kube/config
 
-mkdir -p /home/ubuntu/.kube
-cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
-chown ubuntu:ubuntu /home/ubuntu/.kube/config
-
 kubectl --kubeconfig=/root/.kube/config apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
-kubeadm token create --print-join-command > /root/kubeadm-join-command.sh
+JOIN_CMD=$(kubeadm token create --print-join-command)
+echo "$JOIN_CMD" > /root/kubeadm-join-command.sh
 chmod 600 /root/kubeadm-join-command.sh
+
+gsutil cp /root/kubeadm-join-command.sh gs://${state_bucket}/k8s/join-command.sh
