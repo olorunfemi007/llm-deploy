ae7bde0a62dcbeadf9271c9ef5f554929576f276
olorunfemi007 <olorunfemikawonise@rocketmail.com>
2026-06-19 13:39:38 -0400

    create nfs server on control plane for dev purposes

--- terraform/scripts/common.sh
+++ terraform/scripts/common.sh
@@ -47,7 +47,7 @@ curl -fsSL "https://pkgs.k8s.io/core:/stable:/v${KUBE_VERSION}/deb/Release.key" 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBE_VERSION}/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet kubeadm kubectl nfs-common
 apt-mark hold kubelet kubeadm kubectl
 
 systemctl enable kubelet
