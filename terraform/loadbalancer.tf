c97b995f7fb6dd0f8e7bd56b1f50c7bc7c511835
olorunfemi007 <olorunfemikawonise@rocketmail.com>
2026-06-20 14:07:42 -0400

    switch to alb from nlb

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -62,4 +62,6 @@ resource "google_compute_forwarding_rule" "llm_http" {
   target                = google_compute_region_target_http_proxy.llm_proxy.id
   load_balancing_scheme = "EXTERNAL_MANAGED"
   network_tier          = "STANDARD"
+
+  depends_on = [google_compute_subnetwork.proxy_only]
 }
